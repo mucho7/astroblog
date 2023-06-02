ec86080a01325d41909563630c9c78b0b9cf80c2
Suhjeong Kim <109324498+kimsj-git@users.noreply.github.com>
2023-06-03 00:10:09 +0900

    Feat: Header 페이지 네비게이션 추가

--- frontend/src/layout/Header.js
+++ frontend/src/layout/Header.js
@@ -1,29 +1,59 @@
-import styled from "styled-components";
-import { lighten } from "polished";
+import styled from "styled-components"
+import { lighten } from "polished"
+import terminal_icon from "../assets/terminal_icon.png"
+
+import { useLocation, useNavigate } from "react-router-dom"
 
 const Header = () => {
-	const inProgress = () => {
-		alert("준비 중입니다. \u{1F30A}\u{1F30A}")
-	}
+  const inProgress = () => {
+    alert("준비 중입니다. \u{1F30A}\u{1F30A}")
+  }
+
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  const handleNav = (page) => {
+    // console.log(location.pathname)
+    if (location.pathname !== page) {
+      switch (page) {
+        case "/project/":
+          navigate("/project/")
+          break
+        case "/":
+          navigate("/")
+          break
+      }
+    }
+  }
 
   return (
     <header>
       <HeaderContainer>
         <FlexItem>
-          <NavItemTypo onClick={inProgress}>ABOUT</NavItemTypo>
+          <img
+            src={terminal_icon}
+            alt="shark"
+            style={{ height: "2.5rem", cursor: "pointer"}}
+            onClick={() => {handleNav("/")}}
+          />
         </FlexItem>
-        <FlexItem>
-          <NavItemTypo onClick={inProgress}>PROJECT</NavItemTypo>
-        </FlexItem>
-        <FlexItem>
-          <CustomLink href="https://github.com/kimsj-git">
-            <NavItemTypo>GITHUB</NavItemTypo>
-          </CustomLink>
-        </FlexItem>
+          <FlexItem>
+            <NavItemTypo onClick={inProgress}>ABOUT</NavItemTypo>
+          </FlexItem>
+          <FlexItem>
+            <NavItemTypo onClick={() => handleNav("/project/")}>
+              PROJECT
+            </NavItemTypo>
+          </FlexItem>
+          <FlexItem>
+            <CustomLink href="https://github.com/kimsj-git">
+              <NavItemTypo>GITHUB</NavItemTypo>
+            </CustomLink>
+          </FlexItem>
       </HeaderContainer>
     </header>
-  );
-};
+  )
+}
 
 const HeaderContainer = styled.div`
   height: 5rem;
@@ -31,19 +61,19 @@ const HeaderContainer = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: space-around;
-`;
+  justify-content: space-between;
+`
 
 const FlexContainerRow = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
-`;
+`
 
 const FlexItem = styled.div`
-  margin: 0 0.5rem 0 0.5rem;
-`;
+  margin: 0 1rem 0 1rem;
+`
 
 const NavItemTypo = styled.p`
   font-family: UhBeeSkyrain;
@@ -55,10 +85,10 @@ const NavItemTypo = styled.p`
   &:hover {
     color: ${lighten(0.5, "#24292f")};
   }
-`;
-
-const CustomLink = styled.a`
-	text-decoration: none;
 `
 
-export default Header;
+const CustomLink = styled.a`
+  text-decoration: none;
+`
+
+export default Header
