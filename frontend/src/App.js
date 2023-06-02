3b9fd19b419f76053eda838f9ca755ef7c5726e3
Suhjeong Kim <109324498+kimsj-git@users.noreply.github.com>
2023-06-03 00:54:33 +0900

    Feat: 로딩이미지 코드 주석처리

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,24 @@
 import "./App.css"
 
+import { useState, useEffect } from "react"
 import { RouterProvider } from "react-router-dom"
 import router from "./pages"
 
 function App() {
+  // const [isLoading, setIsLoading] = useState(true)
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setIsLoading(false)
+  //   }, 1000)
+  // }, [])
+
   return (
     <>
       <RouterProvider router={router} />
     </>
   )
+  // return <>{isLoading ? <p>Loading</p> : <RouterProvider router={router} />}</>
 }
 
 export default App
