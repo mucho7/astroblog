5cc307d4c64216b7440196a0d5808ca74d7b5aac
Suhjeong Kim <kimsj.py@gmail.com>
2024-08-12 21:28:40 +0900

    Astro Theme Cody 테마 적용

---
+++ astro/src/consts.ts
@@ -0,0 +1,79 @@
+// This is your config file, place any global data here.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+type Config = {
+  title: string;
+  description: string;
+  lang: string;
+  profile: {
+    author: string;
+    description?: string;
+  }
+}
+
+type SocialLink = {
+  icon: string;
+  friendlyName: string; // for accessibility
+  link: string;
+}
+
+export const siteConfig: Config = {
+  title: ">_ kimsj.dev",
+  description: "",
+  lang: "ko-KR",
+  profile: {
+    author: "Kim Suhjeong",
+    description: "TossPayments Server Developer"
+  }
+}
+
+/** 
+  These are you social media links. 
+  It uses https://github.com/natemoo-re/astro-icon#readme
+  You can find icons @ https://icones.js.org/
+*/
+export const socialLinks: Array<SocialLink> = [
+  {
+    icon: "mdi:github",
+    friendlyName: "Github",
+    link: "https://github.com/kimsj-git",
+  },
+  {
+    icon: "mdi:linkedin",
+    friendlyName: "LinkedIn",
+    link: "#",
+  },
+  {
+    icon: "mdi:email",
+    friendlyName: "email",
+    link: "mailto:kimsj.py@gmail.com",
+  },
+  {
+    icon: "mdi:rss",
+    friendlyName: "rss",
+    link: "/rss.xml"
+  }
+];
+
+export const NAV_LINKS: Array<{ title: string, path: string }> = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+  {
+    title: "Projects",
+    path: '/projects'
+  },
+  {
+    title: "Archive",
+    path: '/archive'
+  }
+];
